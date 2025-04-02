7e425eaee99a97b6f8dc8813dcfe5bc72509584f
chilledpear <greentriantle.rex10297@gmail.com>
2025-04-02 21:53:16 +0530

    Titty language modele

--- frontend/script.js
+++ frontend/script.js
@@ -13,7 +13,7 @@ function sendMessage() {
     document.getElementById("user-input").value = "";
 
     fetchChatGPTResponse(userInput).then((response) => {
-      displayMessage("Al16z", response);
+      displayMessage("TLM", response);
     });
   }
 }
